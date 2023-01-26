5f66157f6df61722b69cfae256c2a4cefaee5cc6
jerryclarkc0420 <jerryclarkc0420@gmail.com>
2023-01-26 16:55:27 +0800

    first capstone

---
+++ public/scripts/scripts.js
@@ -0,0 +1,14 @@
+// Nav Bar on scroll    
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.documentElement.scrollTop > 20) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-80px";
+//   }
+// }
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
